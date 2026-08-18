03b5a1a7b70480fb308f045b52cca10613976d55
Marvywavy <adebayotitilayo02@gmail.com>
2026-08-18 13:56:43 +0100

    update homepage and navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,29 @@
 import HomeHero from "../components/HomeHero"
 import WhyChooseUs from "../components/WhyChooseUs"
-//import HistorySwiper from "../components/HistorySwiper"
-//import History from "../components/History"
+import HistorySwiper from "../components/HistorySwiper"
+import History from "../components/History"
 import Story from "../components/story"
 import StoryContent from "../components/StoryContent"
-//import Menu from "../components/Menu"
-//import MenuContent from "../components/MenuContent"
+import Menu from "../components/Menu"
+import MenuContent from "../components/MenuContent"
 import Location from "../components/Location"
 import Insight from "../components/Insight"
 
 
 function Home(){
     return(
-        <div className="flex flex-col gap-20 pb-36 ">
+        <div className="flex flex-col gap-20 w-full ">
             <section className="bg-[#F3F4F8] flex flex-col gap-20 pb-25">
                 <HomeHero />
                 <WhyChooseUs />
-                {/*<History />
-                <HistorySwiper />*/}
+                <History />
+                <HistorySwiper />
                 <Story />
                 <StoryContent />
             </section>
 
-            {/*<Menu />
-            <MenuContent />*/}
+            <Menu />
+            <MenuContent />
 
             <section className="bg-[linear-gradient(to_right,#DE5601_50%,#FD7E32_100%)] py-20 w-full ">
                 <Location />
